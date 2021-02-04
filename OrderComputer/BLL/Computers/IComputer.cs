f503d7f3fe51b5b7cae5b86b15c57fe9303c4f65
Андрей <andinikolaev@yandex.ru>
2021-02-05 00:48:32 +0700

    v1

---
+++ OrderComputer/BLL/Computers/IComputer.cs
@@ -0,0 +1,13 @@
+﻿using OrderComputer.DTO;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OrderComputer.BLL.Computers
+{
+    public interface IComputer
+    {
+        string SystemName { get; }
+        Order Execute(Order order);
+    }
+}
